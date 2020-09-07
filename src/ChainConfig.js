636d2c57dfeddcd5338d5b2fd931260c7dc530f1
ofreeteam <team@ofree.vip>
2020-09-07 11:43:20 +0800

    update chain config

--- src/ChainConfig.js
+++ src/ChainConfig.js
@@ -1,34 +1,29 @@
 var config = {
-  core_asset: "CORE",
-  address_prefix: "GPH",
+  core_asset: "NBS",
+  address_prefix: "NBS",
+  //  交易过期时间
   expire_in_secs: 15,
+  //  提案过期时间
   expire_in_secs_proposal: 24 * 60 * 60,
+  //  理事会提案审核周期
   review_in_secs_committee: 24 * 60 * 60,
+  //  各种链参数设置，根据chain id自动初始化
   networks: {
-    BitShares: {
+    //  new bitshares mainnet
+    NewBitShares: {
       core_asset: "NBS",
       address_prefix: "NBS",
       chain_id:
+        //  nbs todo mainnet chain id
         "905413ea3fd7842629fc7a38e81b32603e2a42bca795a95524c95475a7e31404"
     },
-    Muse: {
-      core_asset: "MUSE",
-      address_prefix: "MUSE",
-      chain_id:
-        "45ad2d3f9ef92a49b55c2227eb06123f613bb35dd08bd876f2aea21925a67a67"
-    },
+    //  new bitshares testnet
     Test: {
       core_asset: "NBS",
       address_prefix: "NBS",
       chain_id:
         "905413ea3fd7842629fc7a38e81b32603e2a42bca795a95524c95475a7e31404"
     },
-    Obelisk: {
-      core_asset: "GOV",
-      address_prefix: "FEW",
-      chain_id:
-        "1cfde7c388b9e8ac06462d68aadbd966b58f88797637d9af805b4560b0e9661e"
-    }
   },
 
   /** Set a few properties for known chain IDs. */
@@ -51,15 +46,15 @@ var config = {
   },
 
   reset: () => {
-    config.core_asset = "CORE";
-    config.address_prefix = "GPH";
+    config.core_asset = "NBS";
+    config.address_prefix = "NBS";
     config.expire_in_secs = 15;
     config.expire_in_secs_proposal = 24 * 60 * 60;
 
     console.log("Chain config reset");
   },
 
-  setPrefix: (prefix = "GPH") => (config.address_prefix = prefix)
+  setPrefix: (prefix = "NBS") => (config.address_prefix = prefix)
 };
 
 export default config;
